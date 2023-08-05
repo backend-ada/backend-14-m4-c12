c9eb8858e86c0b67f58d306ef7a33e792666fb7a
Sol <slereah@gmail.com>
2023-08-05 22:52:08 +0200

    complete el modelo

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
-import * as model from './model/model';
-
-(function main() {
-	// Llamá al método estático uploadNewBook(book: BookData) desde acá y creá nuevas pelis!
-	// Imprimí los resultados en la terminal
-	// Llamá al método estático findBookByTitle(title: string) e imprimí algunos resultados en la terminal.
-})();
+import * as model from "./model/model"
+;(function main() {
+  // Llamá al método estático uploadNewBook(book: BookData) desde acá y creá nuevas pelis!
+  // Imprimí los resultados en la terminal
+  // Llamá al método estático findBookByTitle(title: string) e imprimí algunos resultados en la terminal.
+})()
